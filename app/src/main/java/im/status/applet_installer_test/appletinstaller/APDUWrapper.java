96ef57e0d53a6eaf79e5e2198cce78d80b966443
Andrea Franz <andrea@gravityblast.com>
2018-08-30 18:00:48 +0200

    fix wrapper to encode previous mac as ICV

--- app/src/main/java/im/status/applet_installer_test/appletinstaller/APDUWrapper.java
+++ app/src/main/java/im/status/applet_installer_test/appletinstaller/APDUWrapper.java
@@ -3,16 +3,21 @@ package im.status.applet_installer_test.appletinstaller;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
+import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 
 public class APDUWrapper {
     private byte[] macKeyData;
+    private byte[] icv;
 
     public APDUWrapper(byte[] macKeyData) {
         this.macKeyData = macKeyData;
+        this.icv = Crypto.NullBytes8.clone();
     }
 
     public APDUCommand wrap(APDUCommand cmd) {
@@ -28,16 +33,28 @@ public class APDUWrapper {
             macData.write(data.length + 8);
             macData.write(data);
 
-            byte[] mac = Crypto.macFull3des(this.macKeyData, Crypto.appendDESPadding(macData.toByteArray()), Crypto.NullBytes8);
+            byte[] icv;
+            if (Arrays.equals(this.icv, Crypto.NullBytes8)) {
+                icv = this.icv;
+            } else {
+                icv = Crypto.encryptICV(this.macKeyData, this.icv);
+            }
+
+            byte[] mac = Crypto.macFull3des(this.macKeyData, Crypto.appendDESPadding(macData.toByteArray()), icv);
             byte[] newData = new byte[data.length + mac.length];
             System.arraycopy(data, 0, newData, 0, data.length );
             System.arraycopy(mac, 0, newData, data.length, mac.length );
 
-            APDUCommand wrapped = new APDUCommand(cla, cmd.getIns(), cmd.getP1(), cmd.getP2(), newData);
+            APDUCommand wrapped = new APDUCommand(cla, cmd.getIns(), cmd.getP1(), cmd.getP2(), newData, cmd.getNeedsLE());
+            this.icv = mac.clone();
 
             return wrapped;
         } catch (IOException e) {
             throw new RuntimeException("error wrapping APDU command.", e);
         }
+    }
+
+    public byte[] getICV() {
+        return this.icv;
     }
 }
