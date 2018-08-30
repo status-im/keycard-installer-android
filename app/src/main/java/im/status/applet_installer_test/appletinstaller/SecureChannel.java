b607f66997b3bbbb23050a722a8711abc094e714
Andrea Franz <andrea@gravityblast.com>
2018-08-30 18:01:56 +0200

    add SecureChannel

---
+++ app/src/main/java/im/status/applet_installer_test/appletinstaller/SecureChannel.java
@@ -0,0 +1,22 @@
+package im.status.applet_installer_test.appletinstaller;
+
+import android.provider.Settings;
+
+import java.io.IOException;
+
+public class SecureChannel implements Channel {
+    private Channel channel;
+    private APDUWrapper wrapper;
+
+    public SecureChannel(Channel channel, Keys keys) {
+        this.channel = channel;
+        this.wrapper = new APDUWrapper(keys.getMacKeyData());
+    }
+
+    public APDUResponse send(APDUCommand cmd) throws IOException {
+        Logger.log(String.format("WRAPPING %s %n", HexUtils.byteArrayToHexString(cmd.serialize())));
+        APDUCommand wrappedCommand = this.wrapper.wrap(cmd);
+        Logger.log(String.format("WRAPPED  %s %n", HexUtils.byteArrayToHexString(wrappedCommand.serialize())));
+        return this.channel.send(wrappedCommand);
+    }
+}
