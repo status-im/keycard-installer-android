8678b76612fb5ee9c6b6a059dbf6a479e09dff05
Andrea Franz <andrea@gravityblast.com>
2018-09-10 12:48:20 +0200

    add Dele command and test

--- app/src/main/java/im/status/applet_installer_test/appletinstaller/apducommands/GetData.java
+++
@@ -1,8 +0,0 @@
-package im.status.applet_installer_test.appletinstaller.apducommands;
-
-public class GetData {
-    public static final int CLA = 0x80;
-    public static final int INS = 0x50;
-    public static final int P1 = 0;
-    public static final int P2 = 0;
-}
